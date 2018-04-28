d71129f39f8efe9e43d1626b4cf60f5e08cc6e1e
rizalputraha <rizalputraha@gmail.com>
2018-04-28 19:19:57 +0700

    initial commit version

---
+++ api/routes/firstApiRoute.js
@@ -0,0 +1,14 @@
+'use strict';
+module.exports = function(app){
+    var todolist = require('../controllers/firstApiController'); 
+
+    app.route('/tasks')
+        .get(todolist.list_all_tasks)
+        .post(todolist.create_a_task);
+    
+    app.route('/tasks/:taskId')
+        .get(todolist.read_a_task)
+        .put(todolist.update_a_task)
+        .delete(todolist.delete_a_task);
+    
+};
